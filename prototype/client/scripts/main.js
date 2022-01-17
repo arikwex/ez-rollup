f7873bfb9e1a4ff804e356e76d24b9a95b4f0e5d
Ariel Wexler <ariel@markforged.com>
2022-01-17 15:32:09 -0500

    Little ws

--- prototype/client/scripts/main.js
+++ prototype/client/scripts/main.js
@@ -1,1 +1,6 @@
 console.log('It works!');
+
+const ws = new WebSocket('/connect');
+ws.onopen = () => console.log('Connected!');
+ws.onclose = () => console.warn('Closed');
+ws.onerror = () => console.error('Error');
