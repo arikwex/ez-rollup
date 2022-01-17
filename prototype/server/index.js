928c8c8fcbb1a95d037a45df2991a6cdb63548a1
Ariel Wexler <ariel@markforged.com>
2022-01-17 16:00:11 -0500

    Fix websocket typo

--- prototype/server/index.js
+++ prototype/server/index.js
@@ -1,6 +1,6 @@
 import logger from './logger.js';
 import router from './app/router.js';
-import websockets from './app/websockets.js';
+import websockets from './app/websocket.js';
 
 router.initialize();
 websockets.initialize();
