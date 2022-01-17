475a1bd99b5f0be79807af78533415ff04221c90
Ariel Wexler <ariel@markforged.com>
2022-01-17 15:37:00 -0500

    Basic websocket config included gogo

--- prototype/client/scripts/main.js
+++ prototype/client/scripts/main.js
@@ -1,6 +1,7 @@
 console.log('It works!');
 
-const ws = new WebSocket('/connect');
+const websocketURL = location.origin.replace(/^http:/, 'ws:') + '/connect';
+const ws = new WebSocket(websocketURL);
 ws.onopen = () => console.log('Connected!');
 ws.onclose = () => console.warn('Closed');
 ws.onerror = () => console.error('Error');
